4bc99825b8954e8ad6160ec94ad6f51dc6171938
yochimonji <yochimonji@gmail.com>
2022-09-07 22:33:05 +0900

    add: chapter4

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,13 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import Payment from "./components/Payment.vue"
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="p-6 max-w-lg mx-auto">
+    <Payment></Payment>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
